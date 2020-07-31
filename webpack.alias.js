cf7ff4b54f664ef67fe75da41b98b15683a86c5a
leiyi <leiyibeyond@qq.com>
2020-07-31 16:20:42 +0800

    '添加webpack路径别名识别配置文件'

---
+++ webpack.alias.js
@@ -0,0 +1,16 @@
+/**
+ * webstorm下正确识别路径别名的配置文件
+ * 设置路径：
+ * Settings > Languages & Frameworks > JavaScript > Webpack
+ */
+
+const path = require('path')
+const resolve = dir => path.join(__dirname, '.', dir)
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': resolve('./src')
+    }
+  }
+}
