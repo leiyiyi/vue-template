e37c7f34890cdda22800d9e06bd59463c1dc184b
leiyi <leiyibeyond@qq.com>
2020-08-07 14:42:47 +0800

    vuex自动引入

--- src/apps/dashboard/store/index.js
+++ src/apps/dashboard/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import modules from './modules'
+import { isDev } from '@/common/env'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  strict: isDev,
+  modules,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {}
 })
