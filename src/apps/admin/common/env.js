006915c619fd617c7cce52453f5660560468fc11
leiyi <leiyibeyond@qq.com>
2020-08-11 10:34:17 +0800

    getAppName换一种写法

--- src/apps/admin/common/env.js
+++ src/apps/admin/common/env.js
@@ -1,1 +1,1 @@
-export const appName = /^\.\/src\/apps\/(\w+)\/index\.js$/.exec(require.resolveWeak('../index'))?.[1]
+export const [, appName] = /^\.\/src\/apps\/(\w+)\/index\.js$/.exec(require.resolveWeak('../index')) || []
