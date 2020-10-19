2615b52ca9a0ab7979a421e70f6ff8cb8c1f8172
leiyiyi <leiyibeyond@qq.com>
2020-10-19 17:28:12 +0800

    增加多环境模式打包

--- buildApps.js
+++ buildApps.js
@@ -1,13 +1,21 @@
 /**
  * 应用并行独立打包器
-  */
+ */
 const { info, done, error } = require('@vue/cli-shared-utils')
 const concurrently = require('concurrently')
+const args = require('minimist')(process.argv.slice(2))
 const apps = require('./apps')
-const commands = apps && apps.reduce((pre, cur) => [...pre, `npm:build:app ${cur}`], [])
+const { mode = 'prod' } = args
+const envs = ['dev', 'test', 'stg', 'prod']
+const commands = apps && apps.reduce((pre, cur) => [...pre, `npm:build:app:${mode} ${cur}`], [])
+
+if (!envs.includes(mode)) {
+  error(`mode ${mode} undefined`)
+  process.exit(1)
+}
 
 // 并行打包
-info(`start building apps: ${apps.join(', ')}`)
+info(`start building apps: ${apps.join(', ')}, under ${mode} mode`)
 concurrently(commands)
   .then(() => {
     done('build success')
